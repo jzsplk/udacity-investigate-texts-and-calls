8fa16e1c072328e30041bf9c94a933efd2df50a0
Alex <jzsplking@gmail.com>
2018-05-21 08:38:42 +0800

    Feature：把submit.zip放到外面了

---
+++ Task1.py
@@ -0,0 +1,27 @@
+"""
+下面的文件将会从csv文件中读取读取短信与电话记录，
+你将在以后的课程中了解更多有关读取文件的知识。
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+phone_numbers = list()
+for text in texts:
+    phone_numbers.append(text[0])
+    phone_numbers.append(text[1])
+for call in calls:
+    phone_numbers.append(call[0])
+    phone_numbers.append(call[1])
+print("There are {} different telephone numbers in the records.".format(len(set(phone_numbers))))
+
+"""
+任务1：
+短信和通话记录中一共有多少电话号码？每个号码只统计一次。
+输出信息：
+"There are <count> different telephone numbers in the records."""
---
+++ Task2.py
@@ -0,0 +1,38 @@
+"""
+下面的文件将会从csv文件中读取读取短信与电话记录，
+你将在以后的课程中了解更多有关读取文件的知识。
+"""
+import csv
+with open('texts.csv', 'r') as f:
+    reader = csv.reader(f)
+    texts = list(reader)
+
+with open('calls.csv', 'r') as f:
+    reader = csv.reader(f)
+    calls = list(reader)
+
+"""
+任务2: 哪个电话号码的通话总时间最长? 不要忘记，用于接听电话的时间也是通话时间的一部分。
+输出信息:
+"<telephone number> spent the longest time, <total time> seconds, on the phone during
+September 2016.".
+
+提示: 建立一个字典，并以电话号码为键，通话总时长为值。
+这有利于你编写一个以键值对为输入，并修改字典的函数。
+如果键已经存在于字典内，为键所对应的值加上对应数值；
+如果键不存在于字典内，将此键加入字典，并将它的值设为给定值。
+"""
+call_time = dict()
+def found_call_time (records, value):
+    if value[0] in records:
+        records[value[0]] += int(value[3])
+    if value[1] in records:
+        records[value[1]] += int(value[3])
+    else:
+        records[value[0]] = int(value[3])
+        records[value[1]] = int(value[3])
+for call in calls:
+    found_call_time(call_time, call)
+target = max(zip(call_time.values(), call_time.keys()))
+# print(target)
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(target[1], target[0]))
